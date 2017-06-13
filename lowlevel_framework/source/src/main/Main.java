cfff26dfe3220b7fd8b1d9f977954d7d1084f775
theChaoS <florianprott@gmail.com>
2017-06-13 19:57:01 +0200

    nochbugsdrion

--- lowlevel_framework/source/src/main/Main.java
+++ lowlevel_framework/source/src/main/Main.java
@@ -7,10 +7,15 @@ import lowlevel.*;
 
 import java.io.File;
 import java.io.FilenameFilter;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
+
+import static java.awt.SystemColor.text;
+
 
 /**
  * Main class
@@ -19,12 +24,62 @@ import java.util.Set;
  */
 public class Main {
 
-	public static void main(String[] args) {
-				
+	public static void main(String[] args) throws IOException {
+
+		String relPath = "\\lowlevel_framework\\benchmarks\\kiss_files\\";
+		String userDir = System.getProperty("user.dir");
+		String path = userDir+relPath;
+		File folder = new File(path);
+		File[] listOfFiles = folder.listFiles();
+
+		openClusterBenchmakrFile();
+
+		for (File file : listOfFiles) {
+			String file_name = file.getName();
+			if(file_name.endsWith(".kiss2")|| file_name.endsWith(".kiss")){
+				String input_file_name = userDir+relPath+file_name;
+				Parser p = new Parser();
+				p.parseFile(input_file_name);
+				ParsedFile fsm = p.getParsedFile();
+				StateMachine myFSM = new StateMachine(fsm);
+
+				List<Cluster> clusterBefore = new ArrayList<Cluster>(myFSM.getClusters());
+
+			//	System.out.println(" starting Simulated Annealing");
+				SimulatedAnnealing sa = new SimulatedAnnealing();
+				ClusterFitnessFunction ff = new ClusterFitnessFunction(fsm.getNumInputs());
+
+
+				List<Cluster> best_result=null;
+				// BUGFIX :D
+				for(int i=0;i<100;i++) {
+					int retVar = 0;
+					List<Cluster> result = null;
+					while (retVar == 0) {
+						result = sa.findClustering(fsm, ff, 1);
+						retVar = result.size(); //irgendein Bug den ich nicht verbrochen habe ist hier drin.Bugfix
+					}
+					if(best_result==null || result.size()<best_result.size()){
+						best_result=result;
+					}
+				}
+				saveClusterBenchmark(file_name, best_result, clusterBefore);
+
+				//if()
+			//	printClusterList(clusterBefore);
+				break;
+			//	saveClusterBenchmark(file_name, result, clusterBefore);
+			}
+		}
+
+
+		//keine Zeit mehr das ding zu verstehen oder zu reperaieren
+		/*
 		if(args.length>0){
 			System.out.println(" Current working directory : " + System.getProperty("user.dir"));
 			
 			String input_file_name = args[0];
+			String algo_name = input_file_name;
 			String relPath = "\\lowlevel_framework\\benchmarks\\kiss_files\\";
 			String userDir = System.getProperty("user.dir");
 			String path = userDir+relPath;
@@ -70,11 +125,22 @@ public class Main {
 			
 			// Representation of the FSM
 			ParsedFile fsm = p.getParsedFile();
-			System.out.println(fsm);
+		//	System.out.println(fsm);
 			
 
 			
 			// TODO - here you go
+
+			StateMachine ourFSM = new StateMachine(fsm);
+			List<Cluster> clusterBefore = new ArrayList<Cluster>(ourFSM.getClusters());
+			System.out.println(" starting Simulated Annealing");
+			SimulatedAnnealing sa = new SimulatedAnnealing();
+			ClusterFitnessFunction ff = new ClusterFitnessFunction(fsm.getNumInputs());
+			List<Cluster> result = sa.findClustering(fsm, ff, 1);
+
+			saveClusterBenchmark(algo_name, result, clusterBefore);
+
+			/*
 			System.out.println(" starting Simulated Annealing");
 			SimulatedAnnealing sa = new SimulatedAnnealing();
 			ClusterFitnessFunction ff = new ClusterFitnessFunction(fsm.getNumInputs());
@@ -93,7 +159,7 @@ public class Main {
 		else{
 			System.out.println("No input argument given");
 		}
-		/*
+
 		System.out.println("Beginning Automation");
 		String[] params = {};
 		AutomaticEvaluator aut = new AutomaticEvaluator();
@@ -102,9 +168,54 @@ public class Main {
 		aut.setOutputPath(userDir+"\\abc_output");
 		System.out.println(userDir);
 		aut.automatedAnalysis(userDir, params);
+
 		*/
 	}
 
+	public static void openClusterBenchmakrFile() {
+		String text = "Algorithm,LUT All-One-Hot,LUT Cluster,FF All-One-Hot,FF Cluster";
+		try {
+			writeIntoBenchmakrFile(text);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void writeIntoBenchmakrFile(String text) throws IOException {
+		String relPath = "\\lowlevel_framework\\benchmarks\\kiss_files\\";
+		String userDir = System.getProperty("user.dir");
+		String path = userDir+relPath;
+
+		Path target = Paths.get(path+"output.csv");
+
+		Path file = Files.createFile(target); //BUUUUUUGGGGG BUG
+
+		Files.write(file, text.getBytes(), StandardOpenOption.WRITE);
+	}
+
+	public static void saveClusterBenchmark(String algoName, List<Cluster> sorted_clusters, List<Cluster> unsorted_clusters){
+		String text="";
+		text+=algoName;
+	//	System.out.println(algoName); //Algorithm
+		text+=unsorted_clusters.size();
+	//	System.out.println(unsorted_clusters.size()); //LUT All-One-Hot
+		text+=sorted_clusters.size();
+	//	System.out.println(sorted_clusters.size()); //LUT Cluster
+		text+=unsorted_clusters.size()*2;
+	//	System.out.println(unsorted_clusters.size()*2); //FF All-One-Hot
+		int ff_sorted=0;
+		for(Cluster c:sorted_clusters){
+			ff_sorted+=(Math.log(c.getNumberOfStates())/Math.log(2))+1;
+		}
+		text+=unsorted_clusters.size()*2;
+	//	System.out.println(ff_sorted); //FF Cluster
+
+		try {
+			writeIntoBenchmakrFile(text);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void printClusterList(List<Cluster> list){
 		int numStates = 0;
